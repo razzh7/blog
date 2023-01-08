7933adbfceceb252f3692419b84b111e8b448e0c
razzh <razzhavenir@163.com>
2023-01-08 21:34:40 +0800

    chore: footer 新增备案号

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -16,8 +16,12 @@ export default {
     ],
     socialLinks: [{ icon: 'github', link: 'https://github.com/rzhAvenir' }],
     footer: {
-      message: '@xiaohao-blog',
-      copyright: '浙ICP备2022036392号-1'
+      message: '© 2023-xiaohao',
+      copyright: `<div style="width:300px;margin:0 auto;">
+      <span>浙ICP备2022036392号</span>
+      <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33011002016890" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="./img/gongan.png" style="float:left;"/><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">浙公网安备 33011002016890号</p></a>
+    </div>
+  `
     },
     sidebar: {
       '/front/': frontEnd()
